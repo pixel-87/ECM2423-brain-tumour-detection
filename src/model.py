9960f92d643094ad775b944616632651cbfca655
pixel-87 <edwardoliverthomas@gmail.com>
2025-03-03 10:58:33 +0000

    updated model.py

--- src/model.py
+++ src/model.py
@@ -1,23 +1,25 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input, BatchNormalization
+from tensorflow.keras.regularizers import l2
 
 # Define CNN Model
 def create_model():
     model = Sequential([
         Input(shape=(224, 224, 1)),
-        Conv2D(32, (3,3), activation='relu'),
-        MaxPooling2D(2,2),
+        Conv2D(32, (3, 3), activation='relu', kernel_regularizer=l2(0.001)),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.25),
 
-        Conv2D(64, (3,3), activation='relu'),
-        MaxPooling2D(2,2),
-
-        #Conv2D(128, (3,3), activation='relu'),
-        #MaxPooling2D(2,2),
+        Conv2D(64, (3, 3), activation='relu', kernel_regularizer=l2(0.001)),
+        BatchNormalization(),
+        MaxPooling2D(2, 2),
+        Dropout(0.25),
 
         Flatten(),
-        Dense(128, activation='relu'),
-        Dropout(0.5),  # Reduce overfitting
-        Dense(1, activation='sigmoid')  # Output layer for binary classification
+        Dense(128, activation='relu', kernel_regularizer=l2(0.001)),
+        Dropout(0.5),
+        Dense(1, activation='sigmoid')
     ])
 
     # Compile Model
